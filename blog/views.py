8ea0c868e2dae8dcd80ea3eaffcdacaf4f33ee85
gasper112 <gasper112@yahoo.com>
2016-10-23 10:13:47 +0300

    First commit

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 def home(request):
     return render(request, 'home.html',{})
 
 def post_list(request):
-    return render(request, 'post_list.html',{})
+    posts = Post.objects.all().order_by('publish_date')
+    return render(request, 'post_list.html',{'post':posts})
 
 def test_list(request):
     return render(request, 'test_list.html',{})
+
+
 
 
